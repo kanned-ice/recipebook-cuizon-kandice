e24af2c5bae4d93f01ea11cdd101fe7b002c9a0d
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-02-09 13:02:20 +0800

    Created recipes.html

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import recipes
 
 urlpatterns = [
-    path('recipes/', recipes, name="recipes")
+    path('recipes/list/', recipes, name="recipes")
 ]
 
 app_name = "ledger"
