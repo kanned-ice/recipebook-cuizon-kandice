ec4af9527ce4aa193c64e09d295180de7d496deb
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-02-09 14:13:49 +0800

    Removed recipe2.html, renamed recipe1.html to recipe_list.html, updated views.py and urls.py and finished recipe_list.html

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import recipes
+from .views import recipes, recipe1, recipe2
 
 urlpatterns = [
-    path('recipes/list/', recipes, name="recipes")
+    path('recipes/list/', recipes, name="recipes"),
+    path('recipe/1/', recipe1, name="recipe1"),
+    path('recipe/2/', recipe2, name="recipe2"),
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -65,14 +65,76 @@ recipe = [
             }
         ]
 
+first_recipe = {
+    "name": "Recipe 1",
+    "ingredients": [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ],
+    "link": "/recipe/1"
+}
+
+second_recipe = {
+    "name": "Recipe 2",
+    "ingredients": [
+        {
+            "name": "garlic",
+            "quantity": "1 head"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "vinegar",
+            "quantity": "1/2cup"
+        },
+        {
+            "name": "water",
+            "quantity": "1 cup"
+        },
+        {
+            "name": "salt",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "whole black peppers",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "pork",
+            "quantity": "1 kilo"
+        }
+    ],
+    "link": "/recipe/2"
+}
+
 def recipes(request):
     ctx = {"recipes": recipe}
     return render(request, "ledger/recipes.html", ctx)
 
-class RecipeView(TemplateView):
-    template_name = 'recipes.html'
+def recipe1(request):
+    ctx = {"recipe": first_recipe}
+    return render(request, "ledger/recipe_list.html", ctx)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['recipes'] = recipe.objects.filter(name="Recipe 1")
-        return context
+def recipe2(request):
+    ctx = {"recipe": second_recipe}
+    return render(request, "ledger/recipe_list.html", ctx)
