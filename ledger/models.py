1035d6659da18bd5b530e8e12b1e03aca1c1bbac
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-03-02 15:48:13 +0800

    Added Profile model and modified Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+
+class Profile(models.Model):
+   user = models.OneToOneField(
+       User,
+       on_delete=models.CASCADE
+       )
+  
+   name = models.CharField(max_length=50)
+   short_bio = models.TextField(validators=[MinLengthValidator(255,
+   'the field must contain at least 255 characters')])
 
 
 class Ingredient(models.Model):
@@ -13,13 +26,23 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=50)
+   name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
+   author = models.ForeignKey(
+       Profile,
+       on_delete=models.CASCADE,
+       related_name='recipe_author'
+   )
 
-    def get_absolute_url(self):
-        return reverse('ledger:recipe_detail', args=[str(self.id)])
+   created_on = models.DateTimeField(auto_now_add=True)
+
+   update_on = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+       return self.name
+
+   def get_absolute_url(self):
+       return reverse('ledger:recipe_detail', kwargs={'pk':self.pk})
 
 
 class RecipeIngredient(models.Model):
