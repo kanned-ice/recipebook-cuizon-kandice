0b027d3e4bbd9a3a2df9708e49e4feb647cd98bd
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-03-12 10:29:53 +0800

    Modified Recipe admin to include RecipeImage, and added a static folder with images for testing

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import Recipe, RecipeIngredient, Profile
+from .models import Recipe, RecipeIngredient, RecipeImage, Profile
 
 
 class ProfileInline(admin.StackedInline):
@@ -13,13 +13,17 @@ class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline,]
 
 
-class RecipeInline(admin.TabularInline):
+class IngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
+
+class ImageInline(admin.TabularInline):
+    model = RecipeImage
 
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeInline, ]
+    inlines = [IngredientInline, ImageInline]
 
 
 admin.site.register(Recipe, RecipeAdmin)
