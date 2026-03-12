92f109eaf99f4642e4d53be193427378847a957d
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-03-12 12:08:03 +0800

    Renamed recipe form template, set up the recipe form page and added link from recipe list page

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView
 
 urlpatterns = [
    path('recipes/list/', RecipeListView.as_view(), name="recipes_list"),
    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
+   path('recipe/add/', RecipeCreateView.as_view(), name="recipe_form")
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
@@ -13,3 +14,8 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "ledger/recipe_detail.html"
     redirect_field_name = None
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['name', 'author']
